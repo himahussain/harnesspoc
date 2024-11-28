2d8a010183020b4691d6635953ab4d35319ff5c5
himahussain <mailsforhima@gmail.com>
2024-11-28 17:31:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def hello_world():
     return 'Hello, World! Welcome to my Harness demo application.'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Note: App Engine runs the application on port 8080 by default.
+    app.run(host='0.0.0.0', port=8080, debug=True)
